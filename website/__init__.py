3276ad2b21a51aa52a07c56026cf1aac752363b5
ATG218 <alextgarbuz@gmail.com>
2022-12-04 19:42:20 -0500

    more setup

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'we9pphofljnksdf;fq9ryu39[p3h5blk3nmbxf[249qht4p'
+    app.config['SECRET_KEY'] = 'we9pphofljnksdffq9ryu39p3h5blk3nmbxf249qht4p'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
