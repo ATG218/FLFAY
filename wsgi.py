0dcef58193b6658fcfca006d431755ca591b96d3
ATG218 <alextgarbuz@gmail.com>
2022-12-09 08:45:09 -0500

    waitress

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from website import app
+from website import create_app
+from flask import Flask
+from waitress import serve
+
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host ='0.0.0.0')
+    serve(app,host='0.0.0.0', port=5000, url_scheme='https')
